1e5f82a47093877bcd79ff8c2c1dbf736c3e94bb
S Tuntipong <STuntipong@stuntipongs-air.staff.wireless.sydney.edu.au>
2023-03-20 14:23:21 +1100

    version02

--- sky_sim.py
+++ sky_sim.py
@@ -1,33 +1,34 @@
 '''A script to simulate a population of objects surroundig M31'''
 
+import math
+import random
+
 # Determine Andromeda location in ra/dec degrees
 # from wikipedia
-ra = '00:42:44.3'
-dec = '41:16:09'
+RA = '00:42:44.3'
+DEC = '41:16:09'
 
 # convert to decimal degrees
-from math import *
+d, m, s = DEC.split(':')
+DEC = int(d)+int(m)/60+float(s)/3600
 
-d, m, s = dec.split(':')
-dec = int(d)+int(m)/60+float(s)/3600
+h, m, s = RA.split(':')
+RA = 15*(int(h)+int(m)/60+float(s)/3600)
+RA = RA/math.cos(DEC*math.pi/180)
 
-h, m, s = ra.split(':')
-ra = 15*(int(h)+int(m)/60+float(s)/3600)
-ra = ra/cos(dec*pi/180)
-
-nsrc = 1_000_000
+NSRC = 1_000_000
 
 # make 1000 stars within 1 degree of Andromeda
-from random import *
-ras = []
-decs = []
-for i in range(nsrc):
-    ras.append(ra + uniform(-1,1))
-    decs.append(dec + uniform(-1,1))
+
+RAS = []
+DECS = []
+for i in range(NSRC):
+    RAS.append(RA + random.uniform(-1,1))
+    DECS.append(DEC + random.uniform(-1,1))
 
 
 # now write these to a csv file for use by my other program
-f = open('catalog.csv','w')
-print("id,ra,dec", file=f)
-for i in range(nsrc):
-    print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
+F = open('catalog.csv','w')
+print("id,ra,dec", file=F)
+with i in range(NSRC):
+    print("{0:07d}, {1:12f}, {2:12f}".format(i, RAS[i], DECS[i]), file=F)
