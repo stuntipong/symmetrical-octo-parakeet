77dc97221843b34503c65b237d878feb07be9ce2
S Tuntipong <STuntipong@stuntipongs-air.staff.wireless.sydney.edu.au>
2023-03-20 14:38:24 +1100

    version02

--- sky_sim.py
+++ sky_sim.py
@@ -1,19 +1,23 @@
-'''A script to simulate a population of objects surroundig M31'''
+'''
+A script to simulate a population of objects surroundig M31
+'''
 
 import math
 import random
 
 # Determine Andromeda location in ra/dec degrees
 # from wikipedia
+
 RA = '00:42:44.3'
 DEC = '41:16:09'
 
 # convert to decimal degrees
+
 d, m, s = DEC.split(':')
 DEC = int(d)+int(m)/60+float(s)/3600
 
-h, m, s = RA.split(':')
-RA = 15*(int(h)+int(m)/60+float(s)/3600)
+h, m, sec = RA.split(':')
+RA = 15*(int(h)+int(m)/60+float(sec)/3600)
 RA = RA/math.cos(DEC*math.pi/180)
 
 NSRC = 1_000_000
@@ -26,9 +30,9 @@ for i in range(NSRC):
     RAS.append(RA + random.uniform(-1,1))
     DECS.append(DEC + random.uniform(-1,1))
 
-
 # now write these to a csv file for use by my other program
-F = open('catalog.csv','w')
-print("id,ra,dec", file=F)
-with i in range(NSRC):
-    print("{0:07d}, {1:12f}, {2:12f}".format(i, RAS[i], DECS[i]), file=F)
+with open('catalog.csv','w',encoding='utf-8') as F:
+    print('id,ra,dec', file=F)
+    for i in range(NSRC):
+        print(f'{i:07d}, {RAS[i]:12f}, {DECS[i]:12f}', file=F)       
+F.close()
