6cb3737b91c8dd38a2ecb11ee1bf696966ac23ee
S Tuntipong <STuntipong@stuntipongs-air.staff.wireless.sydney.edu.au>
2023-03-20 15:19:51 +1100

    version03

--- sky_sim.py
+++ sky_sim.py
@@ -5,34 +5,42 @@ A script to simulate a population of objects surroundig M31
 import math
 import random
 
-# Determine Andromeda location in ra/dec degrees
-# from wikipedia
+def get_radec():
+    '''Determine Andromeda location in ra/dec degrees from wikipedia'''
+    RA = '00:42:44.3'
+    DEC = '41:16:09'
 
-RA = '00:42:44.3'
-DEC = '41:16:09'
+    # convert to decimal degrees
 
-# convert to decimal degrees
+    d, m, s = DEC.split(':')
+    DEC = int(d)+int(m)/60+float(s)/3600
 
-d, m, s = DEC.split(':')
-DEC = int(d)+int(m)/60+float(s)/3600
+    h, m, sec = RA.split(':')
+    RA = 15*(int(h)+int(m)/60+float(sec)/3600)
+    RA = RA/math.cos(DEC*math.pi/180)
+    
+    return (RA,DEC)
 
-h, m, sec = RA.split(':')
-RA = 15*(int(h)+int(m)/60+float(sec)/3600)
-RA = RA/math.cos(DEC*math.pi/180)
+def make_stars(RA,DEC,num_stars):
+    '''make 1000 stars within 1 degree of Andromeda'''
+    #num_stars = 1_000_000
+    RAS = []
+    DECS = []
+    for i in range(num_stars):
+        RAS.append(RA + random.uniform(-1,1))
+        DECS.append(DEC + random.uniform(-1,1))
+    return (RAS,DECS,num_stars)
 
-NSRC = 1_000_000
+def main():
+    RA,DEC = get_radec()
+    RAS,DECS,num_stars = make_stars(RA,DEC,num_stars)
 
-# make 1000 stars within 1 degree of Andromeda
+    # now write these to a csv file for use by my other program
+    with open('catalog.csv','w',encoding='utf-8') as F:
+        print('id,ra,dec', file=F)
+        for i in range(num_stars):
+            print(f'{i:07d}, {RAS[i]:12f}, {DECS[i]:12f}', file=F)       
 
-RAS = []
-DECS = []
-for i in range(NSRC):
-    RAS.append(RA + random.uniform(-1,1))
-    DECS.append(DEC + random.uniform(-1,1))
-
-# now write these to a csv file for use by my other program
-with open('catalog.csv','w',encoding='utf-8') as F:
-    print('id,ra,dec', file=F)
-    for i in range(NSRC):
-        print(f'{i:07d}, {RAS[i]:12f}, {DECS[i]:12f}', file=F)       
-F.close()
+if __name__=='__main__':
+    main()
+            
