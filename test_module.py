d32ca83efc80a074bab9dc1d1a99e5a14dfbb168
S Tuntipong <STuntipong@gmail.com>
2023-03-21 12:27:05 +1100

    version08-tested_tolerance

--- test_module.py
+++ test_module.py
@@ -1,3 +1,6 @@
+import sky_sim
+from numpy import testing
+
 def test_module_import():
     try:
         import sky_sim
@@ -11,3 +14,11 @@ if __name__ == "__main__":
         if f.startswith('test'):
             print(f)
             globals()[f]()
+            
+def test_get_radec():
+    answer = (14.215420962967535, 41.26916666666667)
+    result = sky_sim.get_radec()
+    testing.assert_allclose(answer, result, atol=1./3600)
+    return
+    #if answer != result:
+    #    raise AssertionError(f"Position of M31 Should be {answer}, but got {result}")
